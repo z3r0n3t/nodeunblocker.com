9120b0db91aa682435d0c4eb41b16b163410094f
enp0s10 <cdixon99@ivytech.edu>
2021-03-14 00:45:05 -0500

    Update app.js

--- app.js
+++ app.js
@@ -16,8 +16,24 @@ var unblocker = require('unblocker');
 var Transform = require('stream').Transform;
 var youtube = require('unblocker/examples/youtube/youtube.js')
 var helmet = require("helmet"); //improve security slightly :/ 
+var crypto = require("crypto")
 
+function encrypt(key, data) {
+        var cipher = crypto.createCipher('aes-256-cbc', key);
+        var crypted = cipher.update(data, 'utf-8', 'hex');
+        crypted += cipher.final('hex');
 
+        return crypted;
+}
+
+function decrypt(key, data) {
+        var decipher = crypto.createDecipher('aes-256-cbc', key);
+        var decrypted = decipher.update(data, 'hex', 'utf-8');
+        decrypted += decipher.final('utf-8');
+
+        return decrypted;
+}
+var key = "supersec"
 var app = express();}
 app.use(helmet());
 
